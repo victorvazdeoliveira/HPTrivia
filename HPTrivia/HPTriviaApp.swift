f688410acdaafdf5aeab9b8325d4fe2e61e668e2
Victor Oliveira <70809637+victorvazdeoliveira@users.noreply.github.com>
2024-10-25 15:34:53 -0300

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Victor Vaz de Oliveira on 25/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
