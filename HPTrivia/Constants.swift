62dce3a2ed8354b805e04f7aa62e92e386b590bd
Victor Oliveira <70809637+victorvazdeoliveira@users.noreply.github.com>
2024-10-26 19:05:11 -0300

    First push.

---
+++ HPTrivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HPTrivia
+//
+//  Created by Victor Vaz de Oliveira on 25/10/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
--- HPTrivia/ContentView.swift
+++ HPTrivia/ContentView.swift
@@ -6,16 +6,128 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var scalePlayButton = false
+    @State private var moveBackgroundImage = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack {
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+                    .offset(x: moveBackgroundImage ? geo.size.width/1.1 : -geo.size.width/1.1)
+                    .onAppear {
+                        withAnimation (.linear(duration: 60).repeatForever()) {
+                            moveBackgroundImage.toggle()
+                        }
+
+                    }
+                
+                VStack {
+                    VStack {
+                        Image(systemName: "bolt.fill")
+                            .font(.largeTitle)
+                            .imageScale(.large)
+                        
+                        Text("HP")
+                            .font(.custom(Constants.hpFont, size: 70))
+                            .padding(.bottom, -50)
+                        
+                        Text("Trivia")
+                            .font(.custom(Constants.hpFont, size: 60))
+                    }
+                    .padding(.top, 70)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Recent Scores")
+                            .font(.title2)
+                        
+                        Text("33")
+                        Text("27")
+                        Text("15")
+                    }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.7))
+                    .clipShape(.rect(cornerRadius: 15))
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            // Show instructions screen
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            // Start new game
+                        } label: {
+                            Text("Play")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 50)
+                                .background(.brown)
+                                .clipShape(.rect(cornerRadius: 7))
+                                .shadow(radius: 5)
+                        }
+                        .scaleEffect(scalePlayButton ? 1.2 : 1)
+                        .onAppear{
+                            withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+                                scalePlayButton.toggle()
+                            }
+
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            // Show settings screen
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width)
+                    
+                    Spacer()
+                }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
         }
-        .padding()
+        .preferredColorScheme(.dark)
+        .ignoresSafeArea()
+        .onAppear {
+//            playAudio()
+        }
+    }
+    
+    private func playAudio() {
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
     }
 }
 
